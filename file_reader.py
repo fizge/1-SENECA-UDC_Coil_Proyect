1c26c7da28b1937aaca2a1db7a836e3978c79160
fizge <fiz.garrido@udc.es>
2024-10-08 13:54:37 +0200

    creation of code for reading the data base with Excel, SQLite or  .csv (version1)
    Co-authored-by: PabloDiazBlanco <PabloDiazBlanco@users.noreply.github.com>
    Co-authored-by: CONDESEO <CONDESEO@users.noreply.github.com>

---
+++ file_reader.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import sqlite3
+import os
+
+def leer_csv_o_excel(ruta_archivo):
+    try:
+        if ruta_archivo.endswith(".csv"):
+            datos = pd.read_csv(ruta_archivo)
+        else:
+            datos = pd.read_excel(ruta_archivo)
+        print(datos.head())  
+        return datos
+    except Exception as error:
+        print(f"Error al leer el archivo: {error}")
+
+def leer_sqlite(ruta_bd):
+    try:
+        if not os.path.exists(ruta_bd):
+            print("La base de datos no existe")
+            return None
+
+        conexion = sqlite3.connect(ruta_bd)
+        consulta = "SELECT name FROM sqlite_master WHERE type='table';"
+        nombre_tabla = pd.read_sql_query(consulta, conexion).iloc[0]['name']
+        datos = pd.read_sql_query(f"SELECT * FROM {nombre_tabla}", conexion)
+        print(datos.head()) 
+        return datos
+    except Exception as error:
+        print(f"Error al leer la base de datos: {error}")
+
+def importar_datos(ruta_archivo):
+    if ruta_archivo.endswith('.csv') or ruta_archivo.endswith('.xlsx') or ruta_archivo.endswith('.xls'):
+        return leer_csv_o_excel(ruta_archivo)
+    elif ruta_archivo.endswith('.sqlite') or ruta_archivo.endswith('.db'):
+        return leer_sqlite(ruta_archivo)
+    else:
+        print("Formato de archivo no soportado")
+
+arch =input("Introduzca el archivo: ")
+datos = importar_datos(arch)
