a12a1f2aa30af03fa2d7b4e184c69b56dbfcf9b2
fizge <fiz.garrido@udc.es>
2024-10-13 20:10:35 +0200

    actualization of file_reader

--- src/file_reader.py
+++ src/file_reader.py
@@ -8,37 +8,34 @@ def read_csv_or_excel(file_path):
             data = pd.read_csv(file_path)
         else:
             data = pd.read_excel(file_path)
-        print(data.head())
         return data
-    except Exception as error:
-        print(f"Error reading the file: {error}")
+    except Exception as e:
+        return None
     except FileNotFoundError:
-        print("File not found")
+        return None
     except pd.errors.ParserError:
-        print("Error parsing the file")
+        return None
     except pd.errors.EmptyDataError:
-        print("The file is empty")
+        return None
 
 def read_sqlite(db_path):
     try:
         if not os.path.exists(db_path):
-            print("Database does not exist")
             return None
 
         connection = sqlite3.connect(db_path)
         query = "SELECT name FROM sqlite_master WHERE type='table';"
         table_name = pd.read_sql_query(query, connection).iloc[0]['name']
         data = pd.read_sql_query(f"SELECT * FROM {table_name}", connection)
-        print(data.head())
         return data
     except Exception as e:
-        print(f"Unexpected error: {e}")
+        return None
     except FileNotFoundError:
-        print("File not found")
+        return None
     except pd.errors.ParserError:
-        print("Error parsing the file")
+        return None
     except pd.errors.EmptyDataError:
-        print("The file is empty")
+        return None
 
 def import_data(file_path):
     if file_path.endswith('.csv') or file_path.endswith('.xlsx') or file_path.endswith('.xls'):
@@ -46,7 +43,7 @@ def import_data(file_path):
     elif file_path.endswith('.sqlite') or file_path.endswith('.db'):
         return read_sqlite(file_path)
     else:
-        print("File format not supported")
+        return None
 
 
         
