806abe0a93a718a9e4d4b9b99738e2495e420e4e
fizge <fiz.garrido@udc.es>
2024-10-14 14:03:18 +0200

    format code

--- src/graphic_interface.py
+++ src/graphic_interface.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
-import file_reader as fr 
+import file_reader as fr
+
 
 def show_data(data):
     if data is None or data.empty:
@@ -13,17 +14,20 @@ def show_data(data):
         [sg.Table(values=values, headings=headings, display_row_numbers=True, auto_size_columns=True,
                   num_rows=min(25, len(values)), alternating_row_color='lightblue')]
     ]
-    
+
     window = sg.Window("Data Table", layout, finalize=True)
     window.read()
     window.close()
 
+
 sg.theme('NeutralBlue')
 
 layout = [
-    [sg.Push(),sg.Button('Select dataset', font=('Courier New', 12,'bold'), button_color=('black', 'seagreen'), size=(16, 2), border_width=8),sg.Push()],
-    [sg.InputText(key='-FILE-', size=(60, 1), readonly=True,pad=(10,10))], 
-    [sg.Button('Confirm', font=('Courier New', 10), size=(10, 1),button_color=('black', 'seagreen'), border_width=4)]
+    [sg.Push(), sg.Button('Select dataset', font=('Courier New', 12, 'bold'),
+                          button_color=('black', 'seagreen'), size=(16, 2), border_width=8), sg.Push()],
+    [sg.InputText(key='-FILE-', size=(60, 1), readonly=True, pad=(10, 10))],
+    [sg.Button('Confirm', font=('Courier New', 10), size=(10, 1),
+               button_color=('black', 'seagreen'), border_width=4)]
 ]
 
 window = sg.Window('Select data file', layout)
@@ -33,9 +37,10 @@ while True:
 
     if event == sg.WIN_CLOSED:
         break
-    elif event == 'Select dataset': 
-        file_path = sg.popup_get_file('Select a data file', no_window=True, file_types=(("CSV Files", "*.csv"), ("Excel Files", "*.xls;*.xlsx"), ("SQLite Database", "*.db;*.sqlite")))
-        if file_path:  
+    elif event == 'Select dataset':
+        file_path = sg.popup_get_file('Select a data file', no_window=True, file_types=(
+            ("CSV Files", "*.csv"), ("Excel Files", "*.xls;*.xlsx"), ("SQLite Database", "*.db;*.sqlite")))
+        if file_path:
             window['-FILE-'].update(file_path)
     elif event == 'Confirm':
         file_path = values['-FILE-']
